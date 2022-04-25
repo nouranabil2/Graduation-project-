3ca6d2a53806dfe539f9a9c217c178f576f51dc1
noura <1701595@eng.asu.edu.eg>
2022-04-25 04:43:53 +0200

    adding face recognition module

--- myarabic.py
+++ myarabic.py
@@ -1,4 +1,6 @@
 import cv2
+import datetime
+import os
 from gtts import gTTS
 from playsound import playsound
 import  speech_recognition as sr
@@ -12,12 +14,11 @@ engine = pyttsx3.init('sapi5')
 rate=engine.getProperty('rate')
 engine.setProperty('rate', 150)
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 
 def speak(audio):
 	engine.say(audio)
 	engine.runAndWait()
-
 def formatArabicSentences(sentences):
    formatedSentences = arabic_reshaper.reshape(sentences)
    return get_display(formatedSentences)
@@ -27,6 +28,7 @@ def con(voice):
         obj=gTTS(text=voice,lang='ar',slow=False)
         obj.save('text.mp3')
         playsound('text.mp3')
+        os.remove('text.mp3')
     except:
         return    
 def command():
@@ -57,32 +59,37 @@ def respond(query):
         con("بخير انت كيف حالك")
         return
     if  "سلام"in query:
-        con("الى اللقاء ")
-        return
-    if 'face' in query:
-    #if there_exists(["Face","recignition","FaceRecognition"]):
+        con("لى اللقاء ")
+        exit() 
+
+    if 'من حولي' in query:
+    
           model = FaceRecognition()
-          cam = cv2.VideoCapture(0)
+          #cam = cv2.VideoCapture(0)
           count=0
           a=[]
           flag=0
+          x=0
           #while True:
-          for x in range(3):
+          #for x in range(3):
             
-            ret, image = cam.read()
-            #image=cv2.imread('download.jpg')
-            names,boxes= model.recognize(image)
-            model.draw_boxes(image,names,boxes)
+            #ret, image = cam.read()
+          image=cv2.imread('download.jpg')
+          names,boxes= model.recognize(image)
+          model.draw_boxes(image,names,boxes)
             
             
-            if x==0:
-             for f in names:
+          if x==0:
+            con(" الاشخاص الذين تعرفهم حولك هم") 
+            for f in names:
                if f != 'Unknown' :
                  flag=1
-                 speak(f +"is around you")
+
+                 #speak(f)
+                 con(f)
                  a.insert(count,f) 
                  count=count+1
-            else:
+          else:
              while(len(names)):
                for f in names:
                  for h in a:
@@ -101,14 +108,14 @@ def respond(query):
                 
 
 
-            if flag == 1:
-                speak("those are the people around you")
-            cv2.imshow("image", image) 
-            return
+          if flag == 1:
+                #speak("those are the people around you")
+                cv2.imshow("image", image) 
+                return
             #if cv2.waitKey(1) == 27:  # break if press ESC key
            
                    
-    if'view'in query :
+    if'ماذا حولي'in query :
       yolo = yolo_model()
       #cam = cv2.VideoCapture(0)
       c =0 
@@ -123,15 +130,25 @@ def respond(query):
          speak("and"+pred[x][0][0] )  
         else:   
             c=1
+            con("امامك الان")
             speak( "In front of you there is a"+pred[x][0][0])
           
       speak("That is the full view in front of you  ")   
       return
 
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>= 0 and hour<12:
+		con("صباح الخير")
+        
 
+	else:
+		con("مساء الخير")
 
-voice_data=t=command()
-respond(voice_data)
+wishMe()
+while(1):
+    voice_data=t=command()
+    respond(voice_data)
 #con(t)
 
 
